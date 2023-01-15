4ed83e1e1ce7e0acb931aa9a81f068ec63379682
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-01-15 18:22:22 +0200

    finished

--- src/index.js
+++ src/index.js
@@ -1,46 +1,85 @@
 import './css/styles.css';
-
-
-
-
 import fetchCountries from './fetchCountries'
+import Notiflix from 'notiflix';
+
 
 const debounce = require('lodash.debounce');
-
 const DEBOUNCE_DELAY = 300;
 
-const input = document.querySelector('#search-box')
+const refs = {
+    input: document.querySelector('#search-box'),
+    ulContiner: document.querySelector('.country-list')
+}
 
-input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
-
+refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(evt) {
-    
-    const inputValue = evt.target.value
+    clearMarkup();
+    const inputValue = evt.target.value.trim();
     if (inputValue === '') {
-        console.log('пусто, треба очистити дані')
-        
+        clearMarkup()
         
     } else {
         fetchCountries(inputValue)
             .then(data => {
-                if (data.length === 1) {
-                    console.log('перший варіант', data.length)
-                    console.log(data)
-                
-            }
-                if (data.length > 2 & data.length <= 10) {
-                    console.log('Другий варіант', data.length)
-                    console.log(data)
-                
-                } else {
-                    Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-                    console.log(data)
+                if (data.length <= 10) {
+                   createMarkup(data)
                 }
-                    
-        })
+                if (data.length > 10) 
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');  
+            }          
+        )
     }
-    
+}
+
+function createMarkup (dataArray) {
+    const oneMarkup = obj => {
+        if (dataArray.length < 2) {
+            const languagesList = Object.values(dataArray[0].languages)
+            return `
+            <li class = "list_item">
+                <h2 class = "list_item_title">
+                     <img src="${obj.flags.svg}" alt="${obj.name.common}" width = 70 class = "list_item_img">
+                    ${obj.name.common}
+                </h2>
+                <h3>Capital:   <span class = "list_item_span">${obj.capital}</span></h3>
+                <h3>Population:   <span class = "list_item_span">${obj.population}</span></h3>
+                <h3>Languages:   <span class = "list_item_span">${languagesList}</span></h3>
+            </li>
+            `;
+        } else {
+            return `
+             <li class = "list_item">
+                <h3>
+                    <img src="${obj.flags.svg}" alt="${obj.name.common}" width = 40>
+                    ${obj.name.common}
+                </h3>
+            </li>
+            `;
+        }
+    }
+    const markup = dataArray.map(oneMarkup).join('');
+    refs.ulContiner.insertAdjacentHTML('afterbegin', markup)
+}
+
+function clearMarkup() {
+    refs.ulContiner.innerHTML = ''
 }
 
 
+
+
+
+
+// function createMarkup (arr) {
+//     const markup = arr.map(({ population, name: { common } }) => 
+//      `
+//       <li>
+//         <img src="${population}.flags.png" alt="${common}.common">
+//         <h2>${common}</h2>
+//       </li>
+//       `
+//     ).join('');  
+    
+//     ulContiner.insertAdjacentHTML('afterbegin', markup)
+// }
